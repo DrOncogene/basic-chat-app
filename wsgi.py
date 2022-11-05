8829ec07a8642706533d6d4b0b9a83bd54caf598
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-11-05 11:32:19 +0100

    installed pika for rabbitmq connection

--- wsgi.py
+++ wsgi.py
@@ -10,13 +10,13 @@ from app import create_server
 socketio, app = create_server()
 
 if __name__ == '__main__':
-    PORT = env.get('PORT') if env.get('PORT') else 5000
-    HOST = env.get('HOST') if env.get('HOST') else 'localhost'
+    PORT = env.get('PORT') or 5000
+    HOST = env.get('HOST') or 'localhost'
 
     @socketio.on('chat', namespace='/chat')
     def handle_chat(msg):
         """handles the chat event"""
         emit('chat', msg, broadcast=True, include_self=False)
-        print(msg)
+        # print(msg)
 
     socketio.run(app, host=HOST, port=PORT)
